8261730c1d41d98d742aed8ca933cbb13574631f
Imnmania <niloyb@live.com>
2022-10-12 22:10:09 +0600

    android sensor check working

--- android/app/src/main/kotlin/com/niloy/native_code_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/niloy/native_code_example/MainActivity.kt
@@ -1,6 +1,47 @@
 package com.niloy.native_code_example
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val METHOD_CHANNEL_NAME = "com.niloy.native_code_example/method"
+    private val PRESSURE_CHANNEL_NAME = "com.niloy.native_code_example/pressure"
+
+    private var methodChannel: MethodChannel? = null
+    private var sensorManager: SensorManager? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        /// Setup Channels
+        setupChannels(this, flutterEngine.dartExecutor.binaryMessenger)
+    }
+
+    override fun onDestroy() {
+        teardownChannels()
+        super.onDestroy()
+    }
+
+    private fun setupChannels(context: Context, messenger: BinaryMessenger) {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        methodChannel = MethodChannel(messenger, METHOD_CHANNEL_NAME)
+
+        methodChannel?.setMethodCallHandler { call, result ->
+            if (call.method == "isSensorAvailable") {
+                result.success(sensorManager?.getSensorList(Sensor.TYPE_PRESSURE)?.isNotEmpty())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun teardownChannels() {
+        methodChannel?.setMethodCallHandler(null)
+    }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,37 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const methodChannel =
+      MethodChannel("com.niloy.native_code_example/method");
+  static const pressureChannel =
+      EventChannel("com.niloy.native_code_example/pressure");
+  String _sensorAvailable = "Unknown";
+
+  /// This is where we call the native code from Flutter
+  Future<void> _checkAvailability() async {
+    try {
+      /// "isSensorAvailable" is the same string we used in kotlin
+      /// both must be same, no typo allowed
+      bool available = await methodChannel.invokeMethod("isSensorAvailable");
+      setState(() {
+        _sensorAvailable = available.toString();
+      });
+    } on PlatformException catch (e) {
+      log(e.toString());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +39,19 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Native Code Example"),
         centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(_sensorAvailable),
+            ElevatedButton(
+              onPressed: _checkAvailability,
+              child: const Text("Check for Sensor"),
+            ),
+          ],
+        ),
       ),
     );
   }
